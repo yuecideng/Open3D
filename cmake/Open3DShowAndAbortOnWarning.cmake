58fc10f33d6215231d3ff1e4de374a83dc443aa9
yuecideng <dengyueci@qq.com>
2022-03-26 21:25:15 +0800

    init

--- cmake/Open3DShowAndAbortOnWarning.cmake
+++ cmake/Open3DShowAndAbortOnWarning.cmake
@@ -19,6 +19,7 @@ function(open3d_show_and_abort_on_warning target)
         /wd4245        # signed/unsigned mismatch (visualization, PoissonRecon, ...)
         /wd4267        # conversion from size_t to smaller type (FixedRadiusSearchCUDA, tests)
         /wd4305        # conversion to smaller type in initialization or constructor argument (examples, tests)
+        /wd4819
     )
     set(DISABLE_GNU_CLANG_INTEL_WARNINGS
         -Wno-unused-parameter               # (many places in Open3D code)
